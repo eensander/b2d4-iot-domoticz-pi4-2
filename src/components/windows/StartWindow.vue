be94f2fa9f7e4bf00812794deb43f1798c20b0d7
Dylan Giesberts <dylan.giesberts@gmail.com>
2020-06-07 23:36:06 +0200

    Added Polygons

--- src/components/windows/StartWindow.vue
+++ src/components/windows/StartWindow.vue
@@ -8,6 +8,7 @@
 
             <polygon
                 v-for="room in rooms"
+                v-bind:key="room.name"
                 @click="do_shit(room.name)"
                 :points=room.points
                 class="room-item"  />
@@ -32,9 +33,37 @@ export default Vue.extend({
         return {
             rooms: [
                 {
-                    name: 'room-1',
+                    name: 'WJ.C.',
                     points: '100,100 308,100 308,159 295,159 294,208 100,208',
-                }
+                },
+                {
+                    name: 'MASTER SUITE',
+                    points: '52,214 296,214 296,401 52,401',
+                },
+                {
+                    name: 'MASTER BATH',
+                    points: '313,101 406,101 406,167 432,167 432,312 301,312 301,166 314,166',
+                },
+                {
+                    name: 'STOR. CLOSET',
+                    points: '412,102 532,102 532,206 439,206 439,159 412,159',
+                },
+                {
+                    name: 'MUD RM',
+                    points: '441,216 535,216 535,310 440,310',
+                },
+                {
+                    name: '3-CAR GARAGE',
+                    points: '541,101 825,101 825,329 802,329 802,461 541,461',
+                },
+                {
+                    name: '',
+                    points: '',
+                },
+                {
+                    name: '',
+                    points: '',
+                },
             ]
         }
     },
