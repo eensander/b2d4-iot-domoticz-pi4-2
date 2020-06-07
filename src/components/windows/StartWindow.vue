4f4f3140e358206cb25a27b51041c0af0a75b1fa
eensander <sanderve@protonmail.com>
2020-06-08 00:03:45 +0200

    only move to adjacent rooms

--- src/components/windows/StartWindow.vue
+++ src/components/windows/StartWindow.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="w-full">
 
-		<span style="text-xl font-bold">{{ current_room != null ? current_room.name : 'geen kamer gekozen'  }}</span>
+		<span style="text-xl font-bold">{{ current_room != null ? (current_room.name + '  - can go to - ' + current_room.adjacent_rooms) : 'geen kamer gekozen'  }}</span>
 
         <svg style="width:1000px; height:1000px;" viewBox="0 0 1000 1000">
             <image xlink:href="/floorplan-1.gif" width="100%"  />
@@ -12,7 +12,7 @@
                 @click="room_click(room)"
                 :points=room.points
                 class="room-item"
-                v-bind:class="{ 'room-active': is_active(room) }" />
+                v-bind:class="{ 'room-active': is_active(room), 'room-possible': is_move_possible(room) }" />
         </svg>
 
     </div>
@@ -44,36 +44,57 @@ export default Vue.extend({
                 {
                     name: 'WJ.C.',
                     points: '100,100 308,100 308,159 295,159 294,208 100,208',
+                    adjacent_rooms: [
+                        'MASTER SUITE',
+                        'MASTER BATH'
+                    ],
                 },
                 {
                     name: 'MASTER SUITE',
                     points: '52,214 296,214 296,401 52,401',
+                    adjacent_rooms: [
+                        'WJ.C.',
+                        'MASTER BATH',
+                    ],
                 },
                 {
                     name: 'MASTER BATH',
                     points: '313,101 406,101 406,167 432,167 432,312 301,312 301,166 314,166',
+                    adjacent_rooms: [
+                        'STOR. CLOSET',
+                        'MUD RM',
+                        'WJ.C.',
+                        'MASTER SUITE'
+                    ],
                 },
                 {
                     name: 'STOR. CLOSET',
                     points: '412,102 532,102 532,206 439,206 439,159 412,159',
+                    adjacent_rooms: [
+                        'MUD RM',
+                        'MASTER BATH',
+                        '3-CAR GARAGE'
+                    ],
                 },
                 {
                     name: 'MUD RM',
                     points: '441,216 535,216 535,310 440,310',
+                    adjacent_rooms: [
+                        'STOR. CLOSET',
+                        'MASTER BATH',
+                        '3-CAR GARAGE',
+                    ],
                 },
                 {
                     name: '3-CAR GARAGE',
                     points: '541,101 825,101 825,329 802,329 802,461 541,461',
+                    adjacent_rooms: [
+                        'MUD RM',
+                        'STOR. CLOSET'
+                    ],
                 },
-                {
-                    name: '',
-                    points: '',
-                },
-                {
-                    name: '',
-                    points: '',
-                },
-            ]
+            ],
+
         }
     },
 
@@ -81,13 +102,29 @@ export default Vue.extend({
 
         room_click: function(room: any) {
 
-            this.current_room = room;
-            alert('you clicked ' + room.name);
+            if (this.current_room == room) {
+                return;
+            }
+            else if (this.current_room == null || this.is_move_possible(room))
+            {
+                this.current_room = room;
+                // alert('you clicked ' + room.name);
+            }
+            else
+            {
+                alert('you cant move to this room from your current room');
+
+            }
+
 
         },
 
         is_active: function(room: any): boolean {
-            return room == this.current_room;
+            return this.current_room != null && this.current_room == room;
+        },
+
+        is_move_possible: function(room: any): boolean {
+            return this.current_room != null && this.current_room.adjacent_rooms.includes(room.name);
         }
 
     },
